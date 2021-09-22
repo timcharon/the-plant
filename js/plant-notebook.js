8e8d87f3d6e99bbd860770b9f7bd5ddb5276c5f6
timcharon <timcharon@web.de>
2021-09-22 21:35:15 +0200

    made cinema

---
+++ js/plant-notebook.js
@@ -0,0 +1,57 @@
+{
+  "origin" : ["#intro# #groupdescription# #group# #[act-adjective:#adjective#][act-hated-thing:#hated-thing#]activity# #event#, said the plant."],
+  "activity" : [
+    "are #act-adjective#",
+    "hate #act-hated-thing#"
+  ],
+
+  "intro" : [
+    "But remember,",
+    "Also,",
+    "Don't forget that",
+    "Did you know that",
+    "I wonder if",
+    "Maybe"
+  ],
+  "groupdescription" : [
+    "all",
+    "some",
+    "not all",
+    "most",
+    "the kindest",
+    ""
+  ],
+  "group" : [
+    "humans",
+    "plants",
+    "stones",
+    "cops",
+    "neoliberals",
+    "animals",
+    "adults",
+    "books"
+  ],
+
+
+  "adjective" : [
+    "hypocrits",
+    "ignorant",
+    "dangerous",
+    "violent",
+    "surprisingly kind"
+  ],
+  "hated-thing" : [
+    "what they do",
+    "you",
+    "their job",
+    "our planet",
+    "progressive ideas",
+    "living their lie"
+  ],
+
+  "event" : [
+    "",
+    "on sunny days",
+    "when you look them in the eyes"
+  ]
+}
