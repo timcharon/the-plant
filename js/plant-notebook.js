4f8dd8243851e7e553ef664e86d3ce86c7cf0386
timcharon <timcharon@web.de>
2021-09-24 16:27:05 +0200

    Update plant-notebook.js

--- js/plant-notebook.js
+++ js/plant-notebook.js
@@ -1,27 +1,48 @@
 {
-  "origin" : ["#intro# #groupdescription# #group# #[act-adjective:#adjective#][act-hated-thing:#hated-thing#]activity# #event#, said the plant."],
-  "activity" : [
-    "are #act-adjective#",
-    "hate #act-hated-thing#"
-  ],
+  "origin" : ["#og-intro# #og-group# #[act-adjective:#adjective#][act-hated-thing:#hated-thing#][act-adj-look:#adj-look#][act-dream:#dream#][act-forget:#forget#][act-discover:#discover#]og-activity# #og-event#, said the plant."],
 
+
+  "og-intro" : [
+    "#intro# #groupdescription#",
+    "#intro# #groupdescription#",
+    "#intro# #groupdescription#",
+    "#intro# #groupdescription#",
+    "#intro# #groupdescription#",
+    "#groupdescription.capitalize#",
+    "#groupdescription.capitalize#",
+    "#groupdescription.capitalize#"
+  ],
   "intro" : [
     "But remember,",
     "Also,",
     "Don't forget that",
     "Did you know that",
+    "Someone has told me that",
+    "I heard that",
     "I wonder if",
-    "Maybe"
+    "Maybe",
+    "Sometimes,",
+    "To my surprise,",
+    "Most of the time,",
+    "Well,"
   ],
   "groupdescription" : [
     "all",
     "some",
     "not all",
     "most",
+    "these",
+    "many",
     "the kindest",
-    ""
+    "the prettiest",
+    "doubting",
+    "evil",
+    "hurt",
+    "sentimental"
   ],
-  "group" : [
+
+
+  "og-group" : [
     "humans",
     "plants",
     "stones",
@@ -29,16 +50,37 @@
     "neoliberals",
     "animals",
     "adults",
-    "books"
+    "books",
+    "living beings",
+    "friends",
+    "poets",
+    "birds",
+    "mythical creatures",
+    "trees"
   ],
 
+
+  "og-activity" : [
+    "are #act-adjective#",
+    "hate #act-hated-thing#",
+    "are afraid of #act-hated-thing#",
+    "don't believe in #act-hated-thing#",
+    "look #act-adj-look#",
+    "dream about #act-dream#",
+    "forget #act-forget#",
+    "unexpectedly discover #act-discover#"
+  ],
 
   "adjective" : [
     "hypocrits",
     "ignorant",
     "dangerous",
     "violent",
-    "surprisingly kind"
+    "surprisingly honest",
+    "good singers",
+    "late",
+    "fucked up",
+    "annoying"
   ],
   "hated-thing" : [
     "what they do",
@@ -46,12 +88,140 @@
     "their job",
     "our planet",
     "progressive ideas",
-    "living their lie"
+    "living their lie",
+    "crying in public",
+    "being alone",
+    "us",
+    "love",
+    "the future",
+    "the world"
+  ],
+  "adj-look" : [
+    "silly",
+    "funny",
+    "sad",
+    "cute",
+    "as if #look-asif#"
+  ],
+  "look-asif" : [
+    "they were hiding something",
+    "they wanted to run away",
+    "something incredible would happen",
+    "no one would believe them",
+    "they had seen a demon"
+  ],
+  "dream" : [
+    "something beautiful",
+    "being someone else",
+    "being a star",
+    "the forest",
+    "finally changing their life",
+    "death",
+    "travelling to another world",
+    "the south"
+  ],
+  "forget" : [
+    "to care about #careabout#",
+    "about me",
+    "their purpose",
+    "to tell stories #forgotten-story#",
+    "the concept of the common good"
+  ],
+  "careabout" : [
+    "us",
+    "their children",
+    "love",
+    "our future",
+    "the world",
+    "being together"
+  ],
+  "forgotten-story" : [
+    "",
+    "with a happy ending",
+    "of resistance",
+    "of care",
+    "of friendship",
+    "of silence"
+  ],
+  "discover" : [
+    "a secret door to their suppressed feelings",
+    "their admiration of #admiration#",
+    "the true meaning of love",
+    "their fear of hurting other beings",
+    "a small key to a beautiful treasure"
+  ],
+  "admiration" : [
+    "radical positions",
+    "expropriation",
+    "temporary autonomous zones",
+    "climate activism",
+    "inclusive communication"
   ],
 
-  "event" : [
+
+  "og-event" : [
     "",
-    "on sunny days",
-    "when you look them in the eyes"
+    "",
+    "",
+    "",
+    "",
+    "on a #day-description# day",
+    "when they're #ev-adjective#",
+    "when they're #ev-adjective#",
+    "when they #ev-activity#",
+    "when they #ev-activity#",
+    "when no one #ev-noone#",
+    "when #season# is coming",
+    "#ev-adverb#"
+  ],
+  "day-description" : [
+    "sunny",
+    "rainy",
+    "cloudy",
+    "lonely",
+    "good",
+    "bad",
+    "boring"
+  ],
+  "ev-adjective" : [
+    "in love",
+    "lonely",
+    "sad",
+    "lost",
+    "disappearing",
+    "anxious",
+    "under pressure",
+    "tired",
+    "in pain",
+    "free"
+  ],
+  "ev-noone" : [
+    "is looking",
+    "is expecting it",
+    "cares",
+    "is around"
+  ],
+  "season" : [
+    "spring",
+    "summer",
+    "autumn",
+    "winter",
+    "sunset"
+  ],
+  "ev-activity" : [
+    "meet",
+    "come home",
+    "try to",
+    "need help",
+    "feel like it",
+    "get hurt",
+    "don't want to do what they do"
+  ],
+  "ev-adverb" : [
+    "in the dark",
+    "in the morning",
+    "nowadays",
+    "lately",
+    "these days"
   ]
 }
